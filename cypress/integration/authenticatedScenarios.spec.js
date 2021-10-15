51b5b3ed45196c8e1aba1beab8129604bbf5e137
DavidsonLuna <davidsonluna@gmail.com>
2021-10-15 14:37:55 -0300

    Fix scenario with only

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -24,7 +24,7 @@ describe('Scenarios where authentication is a pre-requirement', () => {
     })
   })
 
-  it.only('logs out', () => {
+  it('logs out', () => {
     cy.visit('/')
     cy.wait('@getNotes')
 
